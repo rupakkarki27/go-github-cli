41fd3ebee66b7783a367e5b577c5e6ce524fb039
rupakkarki27 <rupakkarki123@gmail.com>
2022-04-29 10:32:42 +0545

    init: add a simple function and test

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func AddNumber(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println(AddNumber(1, 2))
+}
---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestAddNumber(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"2+2=4", args{2, 2}, 4},
+		{"wrong", args{2, 3}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AddNumber(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("AddNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
