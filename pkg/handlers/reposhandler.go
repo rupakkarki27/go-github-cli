4281ffa218c80e3d6ceec155342ab1e7e0d3474f
rupakkarki27 <rupakkarki123@gmail.com>
2022-05-11 21:08:29 +0545

    feat: add sort by flag

--- pkg/handlers/reposhandler.go
+++ pkg/handlers/reposhandler.go
@@ -7,15 +7,31 @@ import (
 	"github.com/rupakkarki27/go-github-cli/pkg/api"
 )
 
-func HandleRepoDetails(username string) {
+func HandleRepoDetails(username string, sort_by string) {
 	repos := api.FetchUserRepos(username)
 
-	sort.Slice(repos, func(i, j int) bool {
-		return repos[i].CreatedAt.After(repos[j].CreatedAt)
-	})
+	switch sort_by {
+	case " ":
+		sort.Slice(repos[:], func(i, j int) bool {
+			return repos[i].CreatedAt.After(repos[j].CreatedAt)
+		})
+	case "forks":
+		sort.Slice(repos[:], func(i, j int) bool {
+			return repos[i].ForksCount > repos[j].ForksCount
+		})
+	default:
+		sort.Slice(repos[:], func(i, j int) bool {
+			return repos[i].CreatedAt.After(repos[j].CreatedAt)
+		})
+	}
 
-	fmt.Println("Here are your latest 5 repos:")
-	for index, value := range repos[1:6] {
-		fmt.Printf("%d - %s\n", index+1, value.FullName)
+	fmt.Println("\n----- A summary of your repos -----")
+	for index, value := range repos[0:5] {
+		if sort_by == "forks" {
+
+			fmt.Printf("%d - %d forks - %s\n", index+1, value.ForksCount, value.FullName)
+		} else {
+			fmt.Printf("%d - %s\n", index+1, value.FullName)
+		}
 	}
 }
